38abde3e7bca63a41d56e9e803d19e929cdf0f8b
unknown <rajkumarvankadoth@gmail.com>
2022-05-18 17:26:39 +0000

    day1

---
+++ day1/AddNumbers.java
@@ -0,0 +1,11 @@
+package javademo1;
+
+public class AddNumbers {
+	public static void main (String args[]) {
+		int num1 =20;
+		int num2 =30;
+		int result =num1+num2;
+		System.out.println(result);
+	}
+
+}
